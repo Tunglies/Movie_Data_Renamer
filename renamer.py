965d754ce14e27b921b8fdcf653e1ba920a40f70
Tunglies <Tunglies@outlook.com>
2023-01-25 03:19:50 +0800

    first commit

---
+++ renamer.py
@@ -0,0 +1,120 @@
+import os
+import re
+from pathlib import Path
+from types import NoneType
+
+
+DEBUG = True
+
+SINGLE = [
+    re.compile(i) for i in [
+        "([A-Z]+-[0-9]+)",
+        "[0-9]+-[0-9]+",
+
+        "(?<=@)(.*)",
+        "(?<=\])(.*)",
+
+        "(.*)(?=-C)",
+        "(.*)(?=\.)",
+
+        "(?<=1H)(.*)",
+        "(.*)(?=HHB)"
+        "(?<=1)(.*)(?=HHB)"
+    ]
+]
+
+DOUBLE = [
+    re.compile(r"([A-Z]+)+([0-9]+)"),
+]
+
+MEDIA_TYPES = (".mp4", ".mkv", ".wmv", ".avi")
+
+
+def debug(*msg: str):
+    if DEBUG:
+        print(*msg)
+
+
+def is_media_extension(extension: str) -> bool:
+    return extension in MEDIA_TYPES
+
+
+def format_file_name(name: str) -> str:
+    temp = name.upper()
+    debug(f"[ Input ] {temp}")
+    for rule in SINGLE:
+        search = rule.search(temp)
+        if type(search) != NoneType:
+            temp = search.group()
+            debug(f"<<< Base >>> {temp}")
+    debug(f"<<< SINGLE >>> {temp}")
+
+    for rule in DOUBLE:
+        search = rule.search(temp)
+        if type(search) != NoneType:
+            prefix, suffix = search.groups()
+            suffix = suffix.lstrip("0")
+            temp = f"{prefix}-{suffix}"
+    debug(f"<<< DOUBLE >>> {temp}")
+
+    debug(f"[ Output ] {temp}")
+    return temp
+
+
+class Media:
+    def __init__(self, file_path: str):
+        self.source_path: Path = Path(file_path).absolute()
+        self.source_dir: Path = None
+        self.source_name: str = None
+        self.extension: str = None
+        self.target_name: str = None
+        self.target_dir: Path = None
+        self.target_path: Path = None
+
+    def source_check(self):
+        if not self.source_path.is_file():
+            self.source_path = None
+        return self
+
+    def extension_check(self):
+        if self.source_path:
+            self.source_dir = self.source_path.parent
+            self.extension = self.source_path.suffix.lower()
+        if not self.extension in MEDIA_TYPES:
+            self.source_path = self.source_dir = self.extension = None
+        return self
+
+    def extract_file_name(self):
+        if self.source_path:
+            self.source_name = self.source_path.name.split(self.extension)[0]
+        return self
+
+    def format_file_name(self):
+        if self.source_name:
+            self.target_name = format_file_name(self.source_name)
+            self.target_dir = self.source_dir
+        return self
+
+    def combine_target_path(self):
+        if self.target_name:
+            self.target_path = self.target_dir.joinpath(
+                f"{self.target_name}{self.extension}")
+        return self
+
+    def rename_file(self):
+        if not self.source_path != self.target_path:
+            return self
+        try:
+            os.rename(self.source_path, self.target_path)
+        except Exception as e:
+            debug(e)
+
+
+def run(file):
+    (Media(file).source_check().extension_check().extract_file_name(
+    ).format_file_name().combine_target_path().rename_file())
+
+
+if __name__ == "__main__":
+    for file in Path(root=__file__).glob("*.*"):
+        run(file)
---
+++ test_renamer.py
@@ -0,0 +1,60 @@
+import unittest
+import renamer
+
+
+renamer.DEBUG = False
+
+
+class RuleCase(unittest.TestCase):
+    def base_assert(self, kwargs):
+        for expect_value, in_value in kwargs.items():
+            self.assertEqual(
+                renamer.format_file_name(in_value),
+                expect_value)
+
+    def test_0(self):
+        in_and_expect = {
+            "HZGD-139": "hzgd-139.mp4",
+            "ABP-992": "abp-992-c.mp4",
+            "VRTM-089": "VRTM-089.HD.mp4",
+            "LOVE-362": "[bai.du]love-362.mp4", }
+        self.base_assert(in_and_expect)
+
+    def test_1(self):
+        in_and_expect = {
+            "CARIB-1080": "baidu2048.com@062620-001-carib-1080p.mp4",
+            "SSIS-334": "baidu800.com@SSIS-334.mp4",
+            "PPPD-851": "PPPD-851(1080P)@18P2P.mp4",
+            "ATID-403": "BAIDU2048.COM@ATID-403.mp4",
+            "IPX-837": "baidu20s8.com@IPX-837.mp4",
+            "SW-259": "SW-259@THMS.mkv", }
+        self.base_assert(in_and_expect)
+
+    def test_2(self):
+        in_and_expect = {
+            "GETS-100": "GETS-100_HD_CH.mp4", }
+        self.base_assert(in_and_expect)
+
+    def test_3(self):
+        in_and_expect = {
+            "BAD-268": "1hbad00268hhb.wmv",
+            "STARS-145": "stars00145.mp4",
+                         "STARS-174": "STARS174C.mp4", }
+        self.base_assert(in_and_expect)
+
+    def test_4(self):
+        in_and_expect = {
+            "FSDSS-077": "FSDSS-077.1080p.mp4",
+                         "STARS-256": "STARS-256~baidu2p.com.mp4",
+                         "MIAE-327": "MIAE-327~baidu224.com.mp4", }
+        self.base_assert(in_and_expect)
+
+    def test_5(self):
+        in_and_expect = {
+            "NDS-004": "574NDS-004 (填充填充-填充) (Uncensored Leaked) 填充填充 填充填充",
+            "SNIS-850": "SNIS-850 Padding Padding 填充填充 [FHD][HEVC]", }
+        self.base_assert(in_and_expect)
+
+
+if __name__ == "__main__":
+    unittest.main()
